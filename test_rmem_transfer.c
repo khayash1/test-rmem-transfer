d9bbab1e3b4c38f0a476d7dbe8eec4f4a0259238
Kunihiko Hayashi <hayashi.kunihiko@socionext.com>
2021-08-24 14:28:48 +0900

    test-rmem-transfer: Use DMA device to allocate memory
    The device of test-rmem-transfer doesn't have IOMMU support, so reserved
    DMA memory allocated from test-rmem-transfer can't be translated by IOMMU
    and dmaengine will causes some issues.
    
    Allocate reserved DMA memory from the device of dmaengine.
    
    Signed-off-by: Kunihiko Hayashi <hayashi.kunihiko@socionext.com>

--- test_rmem_transfer.c
+++ test_rmem_transfer.c
@@ -14,6 +14,7 @@
 #include <linux/of_reserved_mem.h>
 #include <linux/platform_device.h>
 #include <linux/random.h>
+#include <linux/version.h>
 
 static unsigned int test_buf_size = 16384;
 module_param(test_buf_size, uint, S_IRUGO | S_IWUSR);
@@ -23,37 +24,29 @@ static unsigned int test_type = 3;
 module_param(test_type, uint, S_IRUGO | S_IWUSR);
 MODULE_PARM_DESC(test_type, "Type of test (1=dma only, 2=cpu only, 3=both");
 
-static int test_memcpy_dma(struct device *dev,
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 10, 0)
+#define dmaengine_get_dma_device(c) ((c)->device->dev)
+#endif
+
+static int test_memcpy_dma(struct dma_chan *chan,
 			   dma_addr_t dst, dma_addr_t src, size_t len)
 {
-	struct dma_chan *chan;
+	struct device *dev = dmaengine_get_dma_device(chan);
 	struct dma_async_tx_descriptor *tx;
 	dma_cookie_t cookie;
-	dma_cap_mask_t mask;
 	enum dma_status status;
 	enum dma_ctrl_flags flags = DMA_PREP_INTERRUPT | DMA_CTRL_ACK;
-	int ret = 0;
-
-	dma_cap_zero(mask);
-	dma_cap_set(DMA_MEMCPY, mask);
-	chan = dma_request_channel(mask, NULL, NULL);
-	if (!chan) {
-		dev_err(dev, "Failed to request dma channel\n");
-		return -ENODEV;
-	}
 
 	tx = dmaengine_prep_dma_memcpy(chan, dst, src, len, flags);
 	if (!tx) {
 		dev_err(dev, "Failed to prepare dma\n");
-		ret = -ENODEV;
-		goto out;
+		return -ENODEV;
 	}
 
 	cookie = dmaengine_submit(tx);
 	if (dma_submit_error(cookie)) {
 		dev_err(dev, "Failed to submit dma\n");
-		ret = -EINVAL;
-		goto out;
+		return -EINVAL;
 	}
 
 	status = dma_sync_wait(chan, cookie);
@@ -61,14 +54,10 @@ static int test_memcpy_dma(struct device *dev,
 
 	if (status != DMA_COMPLETE) {
 		dev_err(dev, "Failed to transfer dma\n");
-		ret = -EIO;
-		goto out;
+		return -EIO;
 	}
 
-out:
-	dma_release_channel(chan);
-
-	return ret;
+	return 0;
 }
 
 static void test_memory_init(u32 *src, u32 *fix, u32 *dst, int len)
@@ -85,56 +74,75 @@ static void test_memory_init(u32 *src, u32 *fix, u32 *dst, int len)
 static int test_rmem_trasnfer_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
+	struct device *chan_dev;
 	struct device_node *np = dev->of_node, *np_f;
 	struct reserved_mem *rmem;
+	struct dma_chan *chan;
 	void *src_addr, *fixmem_addr, *dst_addr;
 	dma_addr_t src_paddr, fixmem_paddr, dst_paddr;
+	dma_cap_mask_t mask;
 	size_t len = test_buf_size;
 	u32 crc1, crc2;
-	int ret;
+	int ret = 0;
 
 	dev_info(dev, "transfer test for reserved-memory\n");
 
+	/* Request DMA channel */
+	dma_cap_zero(mask);
+	dma_cap_set(DMA_MEMCPY, mask);
+	chan = dma_request_channel(mask, NULL, NULL);
+	if (!chan) {
+		dev_err(dev, "Failed to request dma channel\n");
+		return -ENODEV;
+	}
+	chan_dev = dmaengine_get_dma_device(chan);
+
 	/* Reserved DRAM */
-	ret = of_reserved_mem_device_init_by_idx(dev, np, 0);
+	ret = of_reserved_mem_device_init_by_idx(chan_dev, np, 0);
 	if (ret) {
 		dev_err(dev, "No memory-region found for index 0\n");
-		return ret;
+		goto out_release_chan;
 	}
 
 	/* Fixed memory */
 	np_f = of_parse_phandle(np, "memory-region", 1);
 	if (!np_f) {
 		dev_err(dev, "No memory-region found for index 1\n");
-		return -ENODEV;
+		ret = -ENODEV;
+		goto out_release_rmem;
 	}
 	rmem = of_reserved_mem_lookup(np_f);
 	if (!rmem) {
 		dev_err(dev, "Failed to lookup memory-region 1\n");
-		return -ENODEV;
+		ret = -ENODEV;
+		goto out_release_rmem;
 	}
 	if (rmem->size < len) {
 		dev_err(dev, "The size of memory-region 1 not enough\n");
-		return -ENOMEM;
+		ret = -ENOMEM;
+		goto out_release_rmem;
 	}
 	fixmem_paddr = rmem->base;
 
-	src_addr = dmam_alloc_coherent(dev, len, &src_paddr, GFP_KERNEL);
+	src_addr = dma_alloc_coherent(chan_dev, len, &src_paddr, GFP_KERNEL);
 	if (!src_addr) {
 		dev_err(dev, "Failed to alloc 'src' memory\n");
-		return -ENOMEM;
+		ret = -ENOMEM;
+		goto out_release_rmem;
 	}
 
-	dst_addr = dmam_alloc_coherent(dev, len, &dst_paddr, GFP_KERNEL);
+	dst_addr = dma_alloc_coherent(chan_dev, len, &dst_paddr, GFP_KERNEL);
 	if (!dst_addr) {
 		dev_err(dev, "Failed to alloc 'dst' memory\n");
-		return -ENOMEM;
+		ret = -ENOMEM;
+		goto out_free_src;
 	}
 
 	fixmem_addr = devm_memremap(dev, fixmem_paddr, len, MEMREMAP_WC);
 	if (!fixmem_addr) {
 		dev_err(dev, "Failed to map 'fix' memory\n");
-		return -ENOMEM;
+		ret = -ENOMEM;
+		goto out_free_dst;
 	}
 
 	if (!(test_type & 1))
@@ -144,7 +152,7 @@ static int test_rmem_trasnfer_probe(struct platform_device *pdev)
 	test_memory_init(src_addr, fixmem_addr, dst_addr, len);
 
 	/* test DMA src->fix */
-	ret = test_memcpy_dma(dev, fixmem_paddr, src_paddr, len);
+	ret = test_memcpy_dma(chan, fixmem_paddr, src_paddr, len);
 	if (ret) {
 		dev_err(dev, "Failed to transfer src->fix\n");
 		goto test_dma_exit;
@@ -155,7 +163,7 @@ static int test_rmem_trasnfer_probe(struct platform_device *pdev)
 		 (crc1 == crc2) ? "OK" : "NG");
 
 	/* test DMA fix->dst */
-	ret = test_memcpy_dma(dev, dst_paddr, fixmem_paddr, len);
+	ret = test_memcpy_dma(chan, dst_paddr, fixmem_paddr, len);
 	if (ret) {
 		dev_err(dev, "Failed to transfer fix->dst\n");
 		goto test_dma_exit;
@@ -187,8 +195,17 @@ static int test_rmem_trasnfer_probe(struct platform_device *pdev)
 	dev_info(dev, "CPU: fix:%llx -> dst:%llx %s\n", fixmem_paddr, dst_paddr,
 		 (crc1 == crc2) ? "OK" : "NG");
 
- test_cpu_exit:
-	return 0;
+test_cpu_exit:
+out_free_dst:
+	dma_free_coherent(chan_dev, len, dst_addr, dst_paddr);
+ out_free_src:
+	dma_free_coherent(chan_dev, len, src_addr, src_paddr);
+out_release_rmem:
+	of_reserved_mem_device_release(chan_dev);
+out_release_chan:
+	dma_release_channel(chan);
+
+	return ret;
 }
 
 static const struct of_device_id test_rmem_trasnfer_of_match[] = {
